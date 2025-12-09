973615e514d691452ff771d3b6544318cc4fc885
Peccz <philip.hoglund@gmail.com>
2025-12-09 22:39:49 +0100

    Auto-deploy updates (Clean Install Prep)

--- src/config_manager.py
+++ src/config_manager.py
@@ -53,6 +53,7 @@ HARDCODED_SENSORS = {
     "ha_nissan_soc_entity_id": "sensor.leaf_battery_level",
     "ha_nissan_plugged_entity_id": "binary_sensor.leaf_plugged_in",
     "ha_nissan_range_entity_id": "sensor.leaf_range_ac_on",
+    "ha_nissan_last_updated_id": "sensor.leaf_last_updated",
     "nissan_leaf_climate_entity_id": "switch.leaf_climate_control",
     "nissan_leaf_update_entity_id": "button.leaf_update_data",
     "nissan_leaf_odometer_entity_id": "sensor.leaf_odometer" 
@@ -192,6 +193,7 @@ class ConfigManager:
             'ha_nissan_soc_entity_id': HARDCODED_SENSORS['ha_nissan_soc_entity_id'],
             'ha_nissan_plugged_entity_id': HARDCODED_SENSORS['ha_nissan_plugged_entity_id'],
             'ha_nissan_range_entity_id': HARDCODED_SENSORS['ha_nissan_range_entity_id'],
+            'ha_nissan_last_updated_id': HARDCODED_SENSORS.get('ha_nissan_last_updated_id'),
             'climate_entity_id': HARDCODED_SENSORS['nissan_leaf_climate_entity_id'],
             'odometer_entity_id': HARDCODED_SENSORS.get('nissan_leaf_odometer_entity_id'),
             'update_entity_id': HARDCODED_SENSORS['nissan_leaf_update_entity_id'],
